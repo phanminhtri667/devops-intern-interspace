c5e61c11b0eb95d91a8be03b4ee951af3f4d24e5
phanminhtri667 <phanminhtri667@gmail.com>
2025-11-25 15:05:54 +0700

    test

--- lab-combat-gcp/combat-terraform-ansible-basic/terraform/variables.tf
+++ lab-combat-gcp/combat-terraform-ansible-basic/terraform/variables.tf
@@ -2,11 +2,11 @@ variable "project_id"      { default = "devops-workspace-478502" }
 variable "region"          { default = "us-central1" }
 variable "zone"            { default = "us-central1-a" }
 
-variable "vpc_name"        { default = "lab5-vpc-test-deploysh" }
-variable "subnet_name"     { default = "lab5-subnet-test-deploysh" }
+variable "vpc_name"        { default = "vpc-jenkins" }
+variable "subnet_name"     { default = "subnet-jenkins" }
 variable "subnet_cidr"     { default = "10.1.0.0/16" }
 
-variable "vm_name"         { default = "lab5-vm-test-deploysh" }
+variable "vm_name"         { default = "vm-jenkins-cicd" }
 variable "vm_machine_type" { default = "e2-medium" }
 variable "vm_image"        { default = "ubuntu-os-cloud/ubuntu-2204-lts" }
 variable "public_key_path" { default = "/home/pmt/.ssh/id_ed25519.pub" }
